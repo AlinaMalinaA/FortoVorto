c073e662cb747021fd756ff60fe1e4f4256ccd17
Arbaletos <artemioarbaletos@gmail.com>
2018-02-17 19:58:19 +0300

    basic battle done. With damn modifikators!

--- xochi/xochi.py
+++ xochi/xochi.py
@@ -7,7 +7,7 @@ from random import randint as rand
 class Char():
   """Common PRED profile for character"""
 
-  def __init__(self):
+  def __init__(self,randum = 0):
     self.P = 100 #Power, ability to inflict damage
     self.R = 100 #Rate, ablity to make multiple strikes
     self.E = 100 #Endurance, ability to suffer, max Health
@@ -25,7 +25,49 @@ class Char():
     self.pos_x = 0  #X Cord in a Cell
     self.pos_y = 0  #Y Cord in a Cell
     self.cell = None
-    pass
+ 
+    if randum > 0:
+      a_part = rand(25, 75) * randum // 100
+      v_part = randum - a_part
+      r_part = rand(0, 50) * a_part // 100
+      p_part = a_part - r_part
+      d_part = rand(0, 50) * v_part // 100
+      e_part = v_part - d_part
+      self.R = self.c_par(0,r_part)[0]
+      self.P = int(self.c_par(0,r_part)[1])+p_part
+      self.D = self.c_par(0,d_part)[0]
+      self.E = int(self.c_par(0,d_part)[1])+e_part
+      self.H = self.E
+
+  def get_pts(self,val):
+    if val <= 100:
+      return val
+    return val*val/100 - val + 100
+
+  def get_dpts(self,base,val):
+    if val <= 100:
+      return val-base
+    return self.get_pts(val) - self.get_pts(base)
+
+  def c_par(self,base,amount):
+    ret = [0,0]
+    next = base+1
+    while self.get_dpts(base, next) <= amount:
+      next+=1
+      ret[0]+=1
+    ret[1] = round(amount - self.get_dpts(base, next-1),2)
+    return ret
+
+
+  def modu(self, parnam, parval):
+    for mod in self.mods:
+      if mod['TYPE'] == parnam+'_MUL':
+        parval*= mod['VAL']
+      if mod['TYPE'] == parnam+'_ADD':
+        parval+= mod['VAL']
+      if mod['TYPE'] == parnam+'_SET':
+        parval = mod['VAL']
+    return int(parval)
 
   def getA(self):
     """Attack: Ability attack potential of a char"""
@@ -34,33 +76,78 @@ class Char():
   def getV(self):
     """Vitality: Ability to overcome damage"""
     return self.E*self.D
+  
+
+  def getH(self):
+    """return H, even if it's modificated, like a shit!"""
+    return self.modu('H',self.H)
+
+  def getE(self):
+    """return E, even if it's modificated, like a shit!"""
+    return self.modu('E',self.E)
+
+  def getP(self):
+    """return P, even if it's modificated, like a shit!"""
+    return self.modu('P',self.P)
+
+  def getR(self):
+    """return R, even if it's modificated, like a shit!"""
+    return self.modu('R',self.R)
+
+  def getD(self):
+    """return D, even if it's modificated, like a shit!"""
+    return self.modu('D',self.D)
+
+  def getI(self):
+    """return I, even if it's modificated, like a shit!"""
+    return self.modu('I',self.I)
 
   def getStrikes(self):
-    if self.R % self.weapon.AR==0:
-      return self.R // self.weapon.AR
-    return self.R // self.weapon.AR + 1
+    R = self.getR()
+    if R % self.weapon.AR==0:
+      return R // self.weapon.AR
+    return R // self.weapon.AR + 1
   
   def getS_Chance(self):
-    if self.R % self.weapon.AR==0:
+    R = self.getR()
+    if R % self.weapon.AR==0:
       return self.weapon.AC
-    return self.weapon.AC*(self.getStrikes()-1) + self.weapon.AC * ((self.R % self.weapon.AR) / self.weapon.AR) // self.getStrikes()
+    return (self.weapon.AC*(self.getStrikes()-1) + self.weapon.AC * ((R % self.weapon.AR) / self.weapon.AR)) // self.getStrikes()
 
   def getDodges(self):
-    if self.D % self.armor.DP==0:
-      return self.D // self.armor.DP
-    return self.D // self.armor.DP + 1
+    D = self.getD()
+    if D % self.armor.DP==0:
+      return D // self.armor.DP
+    return D // self.armor.DP + 1
   
   def getD_Chance(self):
-    if self.D % self.armor.DP==0:
+    D = self.getD()
+    if D % self.armor.DP==0:
       return self.armor.DC
-    return self.armor.DC*(self.getDodges()-1) + self.armor.DC * ((self.D % self.armor.DP) / self.armor.DP) // self.getDodges()
-  
+    return (self.armor.DC*(self.getDodges()-1) + self.armor.DC * ((D % self.armor.DP) / self.armor.DP)) // self.getDodges()
+ 
+  def move(self,dir):
+    """Change the position of a warrior in his excellent square"""
+    nx = self.pos[0]+dir[0]
+    ny = self.pos[1]+dir[1]
+    if nx >= 0 and nx <= 2 and ny >= 0 and ny <=2:
+      print('Dauras moving!')
+      if self.cell.isfree(nx,ny):
+        self.cell.free(self.pos[0],self.pos[1])
+        self.cell.add(self,nx,ny)
+
+  def flush_mods(self,type):
+    """Option to destroy all modifiers of selected type, ekzemple, to use them unufoje!"""
+    for mod in self.mods:
+      if mod['T']==type:
+         self.mods.remove(mod)
+   
 class Armor():
   """Armor of a Warrior"""
 
   def __init__(self):
-    self.DC = 100 # Defence Chance, DC
-    self.DP = 100 # Defence Potence, DP
+    self.DC = 80 # Defence Chance, DC
+    self.DP = 80 # Defence Potence, DP
     self.AV = 0 # Armor Value, AV
     self.EC = 100 # Evasion Chance
     pass
@@ -73,8 +160,8 @@ class Weapon():
   """Weapon of a Warrior"""
 
   def __init__(self):
-    self.AC = 100 #Attack Chance, AC
-    self.AR = 100 #Attack Rate, AR
+    self.AC = 80 #Attack Chance, AC
+    self.AR = 80 #Attack Rate, AR
     self.AP = 0 #Armor Piercing
     self.AD = 0 #Additional Damage
     self.PM = 1 #Power Modifier
@@ -84,19 +171,11 @@ class Weapon():
     """Atack Modifier - Power Modifier of each sucesfull strike"""
     return self.AR / self.AC
 
-class Statuso():
-  """Iu passivo shito"""
- 
-  def __init__(self):
-    self.nomo = ''  #Nomo de statuso
-    self.TTL = 0    #Kiel longo statuso vivas
-    self.cTTL = 0   #Kiom da tempo restas
- 
 class BQueue():
   """Battle Queue of events"""
 
   def __init__(self):
-    print ('BQueue created!')
+    #print ('BQueue created!')
     self.que = []
     self.curt = 0
     self.nque = queue.Queue()
@@ -104,6 +183,9 @@ class BQueue():
       self.que.append(queue.Queue())
     self.que[0].put({'TYPE':'BATTLE_START','TTL':0})
     self.que[0].put({'TYPE':'PLEN_TURN_START', 'TTL':-1})
+    for i in range(20):
+      self.que[0].put({'TYPE':'TURN_START', 'TTL':-1, 'NUM':i+1})
+      self.que[0].put({'TYPE':'TURN_END', 'TTL':-1, 'NUM':i+1})
 
   
   def add(self, kio, kien):
@@ -111,7 +193,9 @@ class BQueue():
     if kien+self.curt >= 20:
       self.nque.put([kio,kien+self.curt-20])
     else:
+      end = self.que[kien].get()
       self.que[kien].put(kio)
+      self.que[kien].put(end)
 
   def getnext(self):
     """return next event or event"""
@@ -140,15 +224,66 @@ class Cell():
   def __init__(self,x,y):
     self.x = x
     self.y = y
-    self.kamp = [[],[],[]]
+    self.kamp = [[None,None,None],[None,None,None],[None,None,None]]
+    self.ckamp = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+
+  def add(self, kio,x,y):
+    """adds a mistvieho in a cell"""
+    self.kamp[x][y] = kio
+    self.ckamp[x][y] = kio.Name[0]
+    kio.pos = [x,y]
+    kio.cell = self
+    
+  def draw(self,bord):
+    """return an array of strings, that can be drawed like symbold"""
+    ret = ['','','']
+    n = 0
+    if 'n' in bord:
+      ret.append('')
+      if 'w' in bord:
+        ret[0] = ret[0]+'╔'
+      ret[0] = ret[0]+'═══'
+      if 'e' in bord:
+        ret[0] =ret[0]+'╗'
+      n = 1
+    if 'w' in bord:
+      ret[0+n] += '║'
+      ret[1+n] += '║'
+      ret[2+n] += '║'
+    for i in range(3):
+      for j in range(3):
+        ret[j+n] = ret[j+n]+self.ckamp[i][j]
+    if 'e' in bord:
+      ret[0+n] += '║'
+      ret[1+n] += '║'
+      ret[2+n] += '║'
+    if 's' in bord:
+      ret.append('')
+      if 'w' in bord:
+        ret[3+n] = '╚'
+      ret[3+n] += '═══'
+      if 'e' in bord:
+        ret[3+n] += '╝'
+    return ret
+  
+  def isfree(self, x, y):
+    """Check whether cell is free or not"""
+    return self.kamp[x][y]==None
+  
+  def free(self, x, y):
+    """Make the cell free!"""
+    self.kamp[x][y] = None
+    self.ckamp[x][y] = ' '
  
+ 
+
 def atako(anto,ato):
   """Default attack of a mistvieho"""
   ret = {}
   strajkoj = anto.getStrikes()
   prec = anto.getS_Chance()
-  dodgoj = anto.getDodges()
-  dodge = anto.getD_Chance()
+  dodgoj = ato.getDodges()
+  dodge = ato.getD_Chance()
   armor= ato.armor.AV - anto.weapon.AP
   if armor < 0:
     armor = 0
@@ -156,7 +291,9 @@ def atako(anto,ato):
   fin_damage = 0
   ret['DAMAGE'] = damage
   ret['STRIKE_COUNT'] = strajkoj
-  ret['ACC'] = prec
+  ret['DODGE_COUNT'] = dodgoj
+  ret['DODGE_CHANCE'] = dodge
+  ret['ATTACK_CHANCE'] = prec
   ret['STRIKES'] = []
   ret['DODGES'] = []
   for s in range(strajkoj):
@@ -184,20 +321,37 @@ def atako(anto,ato):
       ret['DODGES'].append(dodge_st[:])
       fin_damage+=damage
       ret['STRIKES'][-1]['dmg'] = damage
-    ret['ALL_DAMAGE'] = fin_damage
-    return ret
+  ret['ALL_DAMAGE'] = fin_damage
+  return ret
            
 def Agu(agaro):
   ret = 0
   for ago in agaro:
     if ago["TYPE"] == 'ATTACK':
-      stat =  atako(ago['ANTO'],ago['ATO'])
+      k_list = [0.5,1,2]
+      anto = ago['ANTO']
+      ato = ago['ATO']
+      if anto.cell.x > ato.cell.x:
+        # print ('right to left!')
+        # 'B' is special battle mod. it works only during one attack! It is the mods for range, ekzemple, mifrendo!
+        anto.mods.append({'TYPE':"R_MUL",'VAL':k_list[2-anto.pos[0]],'T':'B'})
+        anto.mods.append({'TYPE':"D_MUL",'VAL':k_list[anto.pos[0]],'T':'B'})
+        ato.mods.append({'TYPE':"R_MUL",'VAL':k_list[ato.pos[0]],'T':'B'})
+        ato.mods.append({'TYPE':"D_MUL",'VAL':k_list[2-ato.pos[0]],'T':'B'})
+      if anto.cell.x < ato.cell.x:
+        #print ('left to right!')
+        anto.mods.append({'TYPE':"R_MUL",'VAL':k_list[anto.pos[0]],'T':'B'})
+        anto.mods.append({'TYPE':"D_MUL",'VAL':k_list[2-anto.pos[0]],'T':'B'})
+        ato.mods.append({'TYPE':"R_MUL",'VAL':k_list[2-ato.pos[0]],'T':'B'})
+        ato.mods.append({'TYPE':"D_MUL",'VAL':k_list[ato.pos[0]],'T':'B'})
+      stat =  atako(anto,ato)
       print (stat)
+      anto.flush_mods('B')
       bq.add({'TYPE':'GET_DAMAGE', 'TTL':0, 'ID':ago['ATO'].Name, 'SRC':ago['ANTO'].Name,'VAL':stat['ALL_DAMAGE']},0)
       ret = stat
-    if ago["TYPE"] == 'GET_DAMAGE':
+    if ago["TYPE"] == 'SUFFER':
       dmg = ago["VAL"]
-      for mod in ago["ANTO"].mods:
+      for mod in ago["CEL"].mods:
         if mod["TYPE"] == "DEFENSE":
           dmg = dmg // 2
       ago["CEL"].H -= dmg
@@ -208,26 +362,35 @@ def Agu(agaro):
 
 def turno():
   """turnkontrolilo"""
+ 
+  def getcel(dick,ink):
+    k = 1
+    for key in dick.keys():
+      if ink == key.lower() or ink== str(k):
+        return (dick[key])
+      k+=1
+    return None
+
+  def spam_char(nomo,cell):
+    heraro[nomo] = Char(randum=400)
+    heraro[nomo].Name = nomo
+    herlist.append(nomo)
+    cell.add(heraro[nomo],1,1)
+
+  
   w_cell = Cell(0,0)
   e_cell = Cell(1,0)
-  hero1 = Char()
-  hero1.Name = "Mistborner"
-  hero1.cell = w_cell
-  hero1.pos = [1,1]
-  hero2 = Char()
-  hero2.Name = "Schmerzborner"
-  hero2.cell = e_cell
-  hero2.pos = [1,1]
-  
+  herlist = []
   heraro = {}
-  heraro[hero1.Name] = hero1
-  heraro[hero2.Name] = hero2
+  spam_char('Mistarch',w_cell)
+  spam_char('Sterber',e_cell)
   
-  print ("Vi estas "+hero1.Name+", kaj vi nenion povas fari. Beadyrinde!")
   global bq
   bq = BQueue()
-  bq.add({'TYPE':'HERO_TURN','TTL':0,'ID':hero1.Name},0)
-  bq.add({'TYPE':'HERO_TURN','TTL':0,'ID':hero2.Name},0)
+ 
+  for c in herlist:
+    bq.add({'TYPE':'HERO_TURN','TTL':0,'ID':c},0)
+  
   ans = ''
   while ans!= 'q':
     ev = bq.getnext()
@@ -240,25 +403,81 @@ def turno():
             cur.mods.remove(mod)
           if mod['TTL']>0:
             mod['TTL']-=1
-      ans = input("Turno de " + cur.Name + "!What to do, mistvieh?\n")
-      ans = ans.lower()
-      if ans == 'a' or ans =='attack':
-        Agu([{'TYPE':'ATTACK','ANTO':heraro[cur.Name],'ATO':heraro[cur.Name]}])
-        print ("%s attacks %s!" % (cur.Name, cur.Name))
-        bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},20)
-      elif ans == 'd' or ans == 'defense':
-        Agu([{'TYPE':'DEFENSE','ANTO':heraro[cur.Name]}])
-        print ("%s defences himself!" % (cur.Name))
-        bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},20)
-      elif ans == 'w' or ans == 'wait':
-        print ("%s waits!" % (cur.Name))
-        bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},10)
-      elif ans == 'l' or ans == 'list':
-        print ("You know nothing!")
-      else:
-        bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},0)
+      ret = True
+      while ret:
+        ans = input("Turno de " + cur.Name + "!What to do, mistvieh?\n")
+        ans = ans.lower()
+        arg = False
+        args = []
+        com = ans.split(' ')
+        if len(com)>1:
+          arg = True
+          args = com[1:]
+        if ans == 'q':
+           ret = False
+           break
+        if com[0] == 'a' or com[0] =='attack':
+          cel = None
+          if arg:
+            cel = getcel(heraro,args[0])
+          if cel==None:
+            cel = cur
+          Agu([{'TYPE':'ATTACK','ANTO':heraro[cur.Name],'ATO':cel}])
+          print ("%s attacks %s!" % (cur.Name, cel.Name))
+          bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},cur.getI())
+          ret = False
+        elif ans == 'd' or ans == 'defense':
+          Agu([{'TYPE':'DEFENSE','ANTO':heraro[cur.Name]}])
+          print ("%s defences himself!" % (cur.Name))
+          bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},cur.getI())
+          ret = False
+        elif ans == 'w' or ans == 'wait':
+          print ("%s waits!" % (cur.Name))
+          bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},cur.getI()//2)
+          ret = False
+        elif com[0] == 'l' or com[0] == 'list':
+          print(arg)
+          if arg:
+            her = getcel(heraro,args[0])
+            print ("%s:\nHealth: %d/%d\nPower: %d\nAttack Rate: %d\nDefense Rate: %d\n" % (her.Name,her.getH(),her.getE(),her.getP(),her.getR(),her.getD()))
+          else:
+            wcell = w_cell.draw(['n','w','s'])
+            ecell = e_cell.draw(['n','e','s'])
+            print(wcell[0]+'╦'+ecell[0])
+            print(wcell[1]+'║'+ecell[1])
+            print(wcell[2]+'║'+ecell[2])
+            print(wcell[3]+'║'+ecell[3])
+            print(wcell[4]+'╩'+ecell[4])
+            i = 1
+            for key in heraro.keys():
+              print ("%d) %s" % (i,key))
+              i+=1
+        elif com[0] =='m' or com[0] == 'move':
+          if arg:
+            dir = [0,0]
+            if 'n' in com[1]:
+              print('North!')
+              dir[1] -= 1
+            if 'e' in com[1]:
+              print('East!')
+              dir[0] += 1
+            if 's' in com[1]:
+              print('South!')
+              dir[1] += 1
+            if 'w' in com[1]:
+              print('West!')
+              dir[0] -= 1
+            bq.add({'TYPE':'HERO_MOVE', 'TTL':0, 'ID':cur.Name,'DIR':dir},0)
+            bq.add({'TYPE':'HERO_TURN', 'TTL':0, 'ID':cur.Name},cur.getI())
+            ret = False
+    elif ev['TYPE'] == 'HERO_MOVE':
+      print(ev['DIR'])
+      heraro[ev['ID']].move(ev['DIR'])
+      
     elif ev['TYPE'] == 'GET_DAMAGE':
       dmg = Agu([{'TYPE':'SUFFER','CEL':heraro[ev['ID']],'SRC':heraro[ev['SRC']],'VAL':ev['VAL']}])
       print ("%d damage dealed to %s by %s!" % (dmg,ev['ID'],ev['SRC']))
+
+
 if __name__ == '__main__':
   turno() 
